12e8b226af337014abe1b73ff630447f4a4a88fc
ruheni <aleruheni@outlook.com>
2021-01-13 11:31:28 +0300

    removed eslint and prettier dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/axios', '@nuxtjs/auth-next'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
