f247aa61d6244f69b9f80a23d50603b89f7cf1fa
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2021-12-30 19:16:12 +0300

    remove unused modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: ['@nuxtjs/axios', '@nuxtjs/auth-next'],
+  modules: [],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
